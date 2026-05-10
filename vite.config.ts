1e9ea3e43136c06a5351b29ddd0a1f8bd45c1603
Darya Lazareva <daryaleys@ya.ru>
2026-05-10 18:02:55 +0300

    update configs & add linters

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from "vite";
+import { fileURLToPath, URL } from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    resolve: {
+        alias: {
+            "@src": fileURLToPath(new URL("./src", import.meta.url)),
+        },
+    },
     server: {
         open: true,
         port: Number(process.env.PORT) || 3000,
