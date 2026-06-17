c602416ac2351215e781553ef5a92f84f2c3b748
Darya Lazareva <daryaleys@ya.ru>
2026-06-17 16:03:48 +0300

    add vitest

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,13 @@ import { fileURLToPath, URL } from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    resolve: {
-        alias: {
-            "@src": fileURLToPath(new URL("./src", import.meta.url)),
-        },
-    },
-    server: {
-        open: true,
-        port: Number(process.env.PORT) || 3000,
-    },
+	resolve: {
+		alias: {
+			"@src": fileURLToPath(new URL("./src", import.meta.url)),
+		},
+	},
+	server: {
+		open: true,
+		port: Number(process.env.PORT) || 3000,
+	},
 });
